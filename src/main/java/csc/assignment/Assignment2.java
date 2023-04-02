394b93ec96b7e975eaf4aef8311de55403f06d6b
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-04-02 22:31:26 +0100

    Java lang

--- src/main/java/csc/assignment/Assignment2.java
+++ src/main/java/csc/assignment/Assignment2.java
@@ -2,6 +2,7 @@ package csc.assignment;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 // java program that runs 20 times asking for csc lecturer names, rank
@@ -9,7 +10,7 @@ import java.util.Scanner;
 public class Assignment2 {
 
     // two dimensional hashmap
-    private final static HashMap<String, Integer> salaries = new HashMap<String, Integer>();\
+    private final static HashMap<String, Integer> salaries = new HashMap<String, Integer>();
 
     private static <E> void print(E data) {
         System.out.println(data);
@@ -20,21 +21,37 @@ public class Assignment2 {
         salaries.put("associate Professor", 2999);
         salaries.put("senior Lecturer", 2499);
         salaries.put("lecturer", 1999);
+        return new Assignment2();
+    }
+
+    public void initAssignmentOperations() throws Exception {
         for (int i = 0; i < 12; i++) {
             Scanner scanner = new Scanner(System.in);
             while (scanner.hasNextLine()) {
                 print("Input lecturer " + i + " name, rank");
                 String[] input = scanner.nextLine().split(",");
-                salaries.put(input[0], input[1]);
+                salaries.put(input[0], Integer.parseInt(input[1]));
             }
         }
-        return new Assignment2();
+        computeData();
     }
 
     private void computeData() {
-        for (iterable_type iterable_element : iterable) {
-
+        print("--------------------------------------");
+        print("Computing data...");
+        int x = 0, i = 0;
+        print("--------------------------------------");
+        for (Map.Entry<String, Integer> set : salaries.entrySet()) {
+            String name = set.getKey();
+            int salary = set.getValue();
+            print(name + " : " + salary);
+            x += salary;
+            i++;
         }
+        int total_salary = x, average_salary = total_salary / i;
+        print("--------------------------------------");
+        print("Total: " + total_salary + " | Average: " + average_salary);
+        print("--------------------------------------");
     }
 
 }
