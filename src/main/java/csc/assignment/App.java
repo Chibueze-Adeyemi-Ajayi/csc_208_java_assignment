7a356579a954b68b5a3db59058cf3faa498392f1
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-04-02 21:43:17 +0100

    Java lang

--- src/main/java/csc/assignment/App.java
+++ src/main/java/csc/assignment/App.java
@@ -1,13 +1,18 @@
 package csc.assignment;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Scanner x = new Scanner(System.in);
+        while (x.hasNextLine()) {
+
+        }
     }
 }
