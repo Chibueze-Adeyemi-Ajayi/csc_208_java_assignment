5075a141acebc395b7b993113f1e58daed65ab58
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-04-02 21:57:06 +0100

    Java lang

--- src/main/java/csc/assignment/App.java
+++ src/main/java/csc/assignment/App.java
@@ -12,14 +12,27 @@ public class App {
         System.out.println(data);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         print("Hello, I'm here to solve CSC 201 Assignment 1.\nI want you to kindly input marital status, seperate each by comma ',' then I'll print the salary and the overall.\nThank you dear");
         Scanner x = new Scanner(System.in);
+        print("--------------------------------------");
+        Assigment1 ass_1 = Assigment1.getInstance();
         while (x.hasNextLine()) {
-            print(x.nextLine());
-            x.close();
+            String[] statuses = x.nextLine().split(",");
+            for (String status : statuses) {
+                int score = ass_1.getSalaryFor(status);
+                print(score);
+            }
+            print("--------------------------------------");
+            print("TOTAL:" + ass_1.getTotalPayment());
+            print("--------------------------------------");
+            try {
+                x.close();
+            } catch (Exception e) {
+            } finally {
+                ass_1.clear();
+            }
         }
-
     }
 }
