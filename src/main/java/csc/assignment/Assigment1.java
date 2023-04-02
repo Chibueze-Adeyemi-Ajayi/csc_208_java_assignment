83658d70c61ea96aefd9a492e80504fcfce8cb89
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-04-02 21:31:16 +0100

    Java lang

--- src/main/java/csc/assignment/Assigment1.java
+++ src/main/java/csc/assignment/Assigment1.java
@@ -1,10 +1,23 @@
 package csc.assignment;
 
+import java.util.HashMap;
+
 /*
 * This Java program receives the users input
 * then pay him or her according to marital status
 */
 
 public class Assigment1 {
+
+    private static HashMap<String, Integer> db = new HashMap<String, Integer>();
+
+    // singleton
+    public static Assigment1 getInstance() {
+        db.put("single", 500);
+        db.put("married", 2000);
+        db.put("seperated", 3000);
+        db.put("divorced", 4000);
+        return new Assigment1();
+    }
 
 }
