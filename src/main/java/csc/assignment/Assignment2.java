616360fde5713e089a546e0055422be3d153edfc
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-04-02 22:06:00 +0100

    Java lang

--- src/main/java/csc/assignment/Assignment2.java
+++ src/main/java/csc/assignment/Assignment2.java
@@ -1,5 +1,15 @@
 package csc.assignment;
 
+import java.util.HashMap;
+
 public class Assignment2 {
+
+    // two dimensional hashmap
+    private final static HashMap<String, HashMap<Integer, Integer>> salaries = new HashMap<String, HashMap<Integer, Integer>>();
+
+    public static Assignment2 getInstance() {
+
+        return new Assignment2();
+    }
 
 }
